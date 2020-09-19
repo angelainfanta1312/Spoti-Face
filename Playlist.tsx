67b7a217f83d1d68fc799c2ec9d3c6a44b6983e6
Benjamin Gao <bgao01@mit.edu>
2020-09-19 10:18:41 -0400

    uncommented Playlist, initial working version

--- Playlist.tsx
+++ Playlist.tsx
@@ -1,49 +1,50 @@
-/*
 const axios = require('axios');
 
-export default function createPlaylist(image base64, smileprob, **facedata, ...  ){
+// export default function createPlaylist(image base64, smileprob, **facedata, ...  ){
     
 
-
+function createPlaylist(){
 //figure out TARGETPARAMS
-[]
+// []
 
 
 
-[]
-(outputs {target_energy, target_danceability...})
+// []
+// (outputs {target_energy, target_danceability...})
 
 
-//handle auth...
-function getUserPass(){
-    auth_token = ""
-    username = "noteaholic"
-    return auth_token, username
-} <-- return baharuser baharpass  (eventually, will have from OAuth2) (Or locally?)
-getCredentials() <-- return credentials (eventually, go fetch from backend)
+    //handle auth...
+    function getUserPass(){
+        let auth_token = "BQB4f_ZVeo-GGupX4GNVd8aVYCqYOTBSNzFNHty7p84thbExJD1nbsQDCIi5NoRIhZoACeSPtUnWBaOkg6zoMr0IogLb87qcIF6nrli-ZPUd6ceyfZaHGG0k871zFhODy_zI82WKu4OWGuf-x2U9T3urFelSwmNZvWIU2bnrut88DASTLk1tegalnRZfvwrrZQZtV9EsX60ROora5xl1_6zdg9Qf6gT8qjU0r-bdl_I7lp4Np_RGiJWwehjCel8lHcmCV1vz3qfz7s1c"
+        let user_name = "noteaholic"
+        return [auth_token, user_name]
+    } 
+    // <-- return baharuser baharpass  (eventually, will have from OAuth2) (Or locally?)
+    // getCredentials() <-- return credentials (eventually, go fetch from backend)
 
-
-auth_token, username = getUserPass()
-playlist_name = "Testing!!"
-playlist_id = 0
-
+    let auth = getUserPass()
+    let auth_token = auth[0]
+    let user_name = auth[1]
+    let playlist_name = "Test Playlist"
+    let playlist_id = 0
+    let topTracks = []
+    let params = {"valence" : 1.0}
 
 //Figure out SEED
      //X topArtists we probably dont need, right
-    topGenres //TODO (maybe)
-    topTracks = getTopUserTracks()
+    // topGenres //TODO (maybe)
+    // topTracks = getTopUserTracks()
 
-    #Gets the uri of the top tracks
-    topTracks = axios.get('https://api.spotify.com/v1/me/top/tracks', {
+    // #Gets the uri of the top tracks
+    axios.get('https://api.spotify.com/v1/me/top/tracks', {
         headers: {
             'Authorization' : 'Bearer ' + auth_token,
         }
     })
     .then((res) => {
-        data = res.data["items"];
-        tracks = []
+        let data = res.data["items"];
         for(var i = 0; i < data.length; i++){
-            tracks.push(data[i]["uri"])
+            topTracks.push(data[i]["uri"])
             console.log(data[i]["uri"])
     }
     })
@@ -51,36 +52,40 @@ playlist_id = 0
         console.error(error)
     })
 
-    #To get emotionally appropriate songs
-    emotionTopTracks = []
+    // #To get emotionally appropriate songs
+    let emotionTopTracks = []
     for(var i = 0; i < topTracks.length; i++){
         
-        split = topTracks[i].split(":")
-        track_id = split[2]
+        let split = topTracks[i].split(":")
+        let track_id = split[2]
         axios.get('https://api.spotify.com/v1/audio-features/' + track_id, {
         headers: {
             'Authorization' : 'Bearer ' + auth_token,
         }
         })
         .then((res) => {
-            console.log(res.data)
+            let features = res.data
+            if(features["valence"] > .5){
+                emotionTopTracks.push(topTracks[i])
+            }
+            // console.log(res.data)
         })
         .catch((error) => {
             console.error(error)
         })
-        if(this is emotionally appropriate)
-            emotionTopTracks.push(data[i]["uri"])
-            console.log(data[i]["uri"])
-
-    emotionTopTracks = {topTracks.filter( for emotionally appropriate songs using targetParams )}
-            (how to do this)
-            for each track in toptracks:
-                features = spotify.getAudioFeatures(track)
-                run cross(targetParams, features)
-                if ^ good enough:
-                    keep
-                else:
-                    toss
+        // if(this is emotionally appropriate)
+        //     emotionTopTracks.push(data[i]["uri"])
+        //     console.log(data[i]["uri"])
+    }
+    // emotionTopTracks = {topTracks.filter( for emotionally appropriate songs using targetParams )}
+    //         (how to do this)
+    //         for each track in toptracks:
+    //             features = spotify.getAudioFeatures(track)
+    //             run cross(targetParams, features)
+    //             if ^ good enough:
+    //                 keep
+    //             else:
+    //                 toss
 
      // use some of emotionTopTracks for seed when generating
 
@@ -89,10 +94,11 @@ playlist_id = 0
 //how to get tracks:
     let size = 20
     let newness = .3 //:[0, 1] indicate fraction of tracks to come from getseed (also recentness?)
-    newTracks = spotifyGetBasedonseed(limit size * newness: seed : (5tracks/genre), targetParams)
-    oldTracks = emtionTopTracks[:(size * (1 - newness))]
+    
+    // newTracks = spotifyGetBasedonseed(limit size * newness: seed : (5tracks/genre), targetParams)
+    // oldTracks = emtionTopTracks[:(size * (1 - newness))]
 
-    createPlaylist ()
+    // createPlaylist ()
     //Creates a playlist
     axios.post('https://api.spotify.com/v1/users/' + user_name + '/playlists', {"name": playlist_name}, {headers: {'Authorization' : 'Bearer ' + auth_token}})
         .then((res) => {
@@ -104,9 +110,9 @@ playlist_id = 0
     })
 
 
-    addTracks(tracks)
+    // addTracks(tracks)
     //Should add all tracks
-    newTracks.forEach(function(track_uri) {
+    emotionTopTracks.forEach(function(track_uri) {
         axios.post('https://api.spotify.com/v1/playlists/' + playlist_id + "/tracks", {"uris":[track_uri]}, {headers: {'Authorization' : 'Bearer ' + auth_token}})
             .then((res) => {
                 console.log(res.data)
@@ -117,20 +123,18 @@ playlist_id = 0
     });
 
 
-    setPicture(pic)
+    // setPicture(pic)
 
     //Should set picture
-    axios.put('https://api.spotify.com/v1/playlists/' + playlist_id + "/images", image_data, {headers: {'Authorization' : 'Bearer ' + auth_token}})
-    .then((res) => {
-        console.log(res.data)
-    })
-    .catch((error) => {
-        console.error(error)
-    })
+    // axios.put('https://api.spotify.com/v1/playlists/' + playlist_id + "/images", image_data, {headers: {'Authorization' : 'Bearer ' + auth_token}})
+    // .then((res) => {
+    //     console.log(res.data)
+    // })
+    // .catch((error) => {
+    //     console.error(error)
+    // })
+// }
+
 }
 
-
-
-
-return a promise? 
-*/
+createPlaylist()
