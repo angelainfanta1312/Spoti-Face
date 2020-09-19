9e3e7779938f78b084b7a32cbb0044f036a355ff
ayw11 <ayw11@duke.edu>
2020-09-18 22:17:51 -0400

    testing commit

--- App.tsx
+++ App.tsx
@@ -15,6 +15,7 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
+        {/* hello */}
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
