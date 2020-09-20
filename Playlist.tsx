2a8d4523e6f270a1f8061c4b03f7247571dee22d
David Buffkin <dbuffkin@andrew.cmu.edu>
2020-09-20 01:48:20 -0400

    Final playlist workflow

--- Playlist.tsx
+++ Playlist.tsx
@@ -28,215 +28,208 @@ export default function createPlaylist(image_data: string, face: any, token){
 
         //for now, 
         sp = .75
-        let params = {valence: sp}
+        let params = {valence: sp, energy: .5}
         console.log("Smile Prob: " + sp)
+        
+        let valenceWeight = .8
+        let energyWeight = 1 - valenceWeight
+        function match(features){
+            let valenceDiff = valenceWeight * (1 - Math.abs(features.valence - params.valence))
+            let energyDiff = energyWeight * (1 - Math.abs(features.energy - params.energy))
+
+            return valenceDiff + energyDiff
+        } //[0, 1] how well features matches params
+
+
+		let playlist_uri = null;
+		let playlist_id = null;
+		let auth_token = token
+		let user_name = null
+		let playlist_name = "Spotiface" //TODO
+		let newness = .3 //:[0, 1]
+		let size = 20 // += variability * 6
         let variability = .15;
 
+        let numSongsNew = Math.floor(newness * size);
+        let numSongsOld = size - numSongsNew;
 
-		//handle auth...
-		function getUserPass(){
-            // let auth_token = "BQDISAsH_cQ-TDBpKTyCKvPkT-8HpgUGJqb4PyQMuDHxsWpBbDBAy7eK2h3mTLDl0qp5_8TejRgKd7mjyHwkggH4uTZfnquCbJeHIsHyjw1kZAyt6f_XjX66-aNuDB0u7Nb7BS77W35MMdWKXHS_y2owdjz89DMmkXlsAWS0WzgEGTqUgXy-Q0h9H53KnXiscbv5xZ2remLwofUHJrb0N3kMMSPHlg9t4j19qXUn5tjri6jvHMyjsH2De5X-IvfAK5VBT3N0QWrSWkUG"
-            auth_token = token
-            let user_name = "noteaholic"
-            return [auth_token, user_name]
-		} 
-		// <-- return baharuser baharpass  (eventually, will have from OAuth2) (Or locally?)
-		// getCredentials() <-- return credentials (eventually, go fetch from backend)
 
-		let auth = getUserPass()
-		let auth_token = auth[0]
-		let user_name = auth[1]
-		let playlist_name = "ben_computer!!"
-		let newness = .3 //:[0, 1] indicate fraction of tracks to come from getseed (also recentness?)
-		let size = 20 //not really though
+        //unfilteredold = [(1x)(1-newness) top, (4x)(1-newness) library] (for x=100)
+                //map each song to list [matchVal, song]
+        // sort(unfilteredold, compare=(a > b if match(a) > match(b))) 
+        // filteredold = first size * newness of unfilteredold
+
+
+
+
+        //BEGIN!
+
+        let unfilteredToptracks = []
+        for(let i = 0; i < 2; i++){
+            await axios.get('https://api.spotify.com/v1/me/top/tracks', {
+                headers: {
+                    'Authorization' : 'Bearer ' + auth_token,
+                },
+                params: {
+                    'limit': 50,
+                    'offset': i * 50
+                }
+            })
+            .then((res) => {
+                let data = res.data["items"];
+                for(var i = 0; i < data.length; i++){
+                    unfilteredToptracks.push(data[i]["uri"])
+                }
+            })
+            .catch((error) => {
+                reject("Couldn't get the top listened tracks: \n" + error)
+                return
+            })
+         }
+
+
+
+        let unfilteredLibtrack = []
+        for(let i = 0; i < 8; i++){
+            await axios.get('https://api.spotify.com/v1/me/tracks', {
+                headers: {
+                    'Authorization' : 'Bearer ' + auth_token,
+                },
+                params: {
+                    'limit': 50,
+                    'offset': i * 50
+                }
+            })
+            .then((res) => {
+                let data = res.data["items"];
+                for(var i = 0; i < data.length; i++){
+                    filteredToptracks.push(data[i]["uri"])
+                }
+            })
+            .catch((error) => {
+                reject("Couldn't get the top library tracks: \n" + error)
+                return
+            })
+         }
         
-        let playlist_uri = null;
-		let playlist_id = null;
-        let topTracks = []
-        
-        await axios.get('https://api.spotify.com/v1/me', {
-            headers: {
-                'Authorization' : 'Bearer ' + auth_token,
-            },
-		})
-		.then((res) => {
-            // console.log(res.data)
-            user_name = res.data["id"];
-            console.log(user_name)
-		})
-		.catch((error) => {
-            //console.error(error)
-            reject("Couldn't get the username")
-            return
-		})
 
-
-		// #Gets the uris of the top tracks
-		await axios.get('https://api.spotify.com/v1/me/top/tracks', {
-            headers: {
-                'Authorization' : 'Bearer ' + auth_token,
-            },
-            params: {
-                'limit': 20,
-            }
-		})
-		.then((res) => {
-            // console.log(res.data)
-            let data = res.data["items"];
-            for(var i = 0; i < data.length; i++){
-                topTracks.push(data[i]["uri"])
-                // console.log(data[i]["uri"])
-		    }
-		})
-		.catch((error) => {
-            console.error(error)
-            console.log("Can't get any tracks")
-            // reject("Couldn't get the top tracks")
-            // return
-		})
-
-		// #To get emotionally appropriate songs from top tracks
-		let emotionTopTracks = []
-		for(var i = 0; i < topTracks.length; i++){
+        let unfilteredold = unfilteredLibtrack.concat(unfilteredToptracks);
+        //prepend with match val, sort 
+        for(var i = 0; i < unfilteredold.length; i++){
 				
-            let split = topTracks[i].split(":")
+            let split = unfilteredold[i].split(":")
             let track_id = split[2]
             await axios.get('https://api.spotify.com/v1/audio-features/' + track_id, {
             headers: {
                     'Authorization' : 'Bearer ' + auth_token,
-            }
+                }
             })
             .then((res) => {
-                    let features = res.data
-                    //console.log(features["valence"])
-                    let valDiff = Math.abs(features["valence"] - params.valence) //use this to sort later
-                    // if(valDiff < variability){
-                    //         emotionTopTracks.push(topTracks[i])
-                    // }
-                    // console.log(features["valence"])
-                    if(features["valence"] < .5){
-                        emotionTopTracks.push(topTracks[i])
-                    }
-                    // //console.log(res.data)
+                    unfilteredold[i] = [match(res.data), unfilteredold[i]]
             })
             .catch((error) => {
-                    //console.error(error)
-                    // reject("Couldn't get the audio features")
-                    // return
+                    reject("Could not get song features: \n" + error)
             })
         }
-        if (emotionTopTracks.length < 1){
-            if(topTracks.length < 1){
-                reject("No songs...?")
+
+        let cmp = (a, b) => (b[0] - a[0]); //CHECK INTO THIS IF MESSED UP
+        unfilteredold.sort(cmp)
+        let filteredold = unfilteredold.slice(0, numSongsOld).map(v => v[1])
+
+
+        let seedTracks = "";
+
+        //Bandagey, but what if ^ empty?
+        if (filteredold.length == 0){
+            if(unfilteredold.length == 0){
+                reject("No songs found whatsoever.")
                 return
             }
-            emotionTopTracks.push(topTracks[0])
+        } else {
+            //console.log(seed_track)
+            for(var i =0; i<5;i++){
+                if(i >= filteredold.length)
+                    break
+                let split = filteredold[i].split(":")
+                let id = split[2]
+                if(i == 0){
+                    seedTracks = id
+                }
+                else{
+                    seedTracks = seedTracks.concat(",", id)
+                }
+            }
         }
-        // //Eventually this will want to sort on how close to target
-		// //Gets the 5 tracks we are seeding with
-		let seed_track = ""
-		//console.log(seed_track)
-		for(var i =0; i<5;i++){
-            if(i >= emotionTopTracks.length)
-                break
-			let split = emotionTopTracks[i].split(":")
-            let id = split[2]
-            if(i == 0){
-                seed_track = id
-            }
-            else{
-                seed_track = seed_track.concat(",", id)
-            }
 
-			//console.log(id)
-		}
-
-		// //console.log(seed_track)
-		
-		// //Creates the playlist
-		await axios.post('https://api.spotify.com/v1/users/' + user_name + '/playlists', {"name": playlist_name, "public": false}, {headers: {'Authorization' : 'Bearer ' + auth_token}})
-            .then((res) => {
-                // console.log(res.data)
-                playlist_id = res.data["id"]
-                playlist_uri = res.data["uri"]
-                // console.log(playlist_id)
-            })
-            .catch((error) => {
-                console.error(error)
-                // reject("Can't create a new playlist")
-                // return
-		})
-
-
-        //gets recommended tracks
-		let newTracks = []
+        
+        let filterednew = []
 		await axios.get('https://api.spotify.com/v1/recommendations', {
 		headers: {
 			'Authorization' : 'Bearer ' + auth_token,
 		},
 		params: {
-			"limit" : newness * size,
-			"seed_tracks" : seed_track,
+			"limit" : numSongsNew,
+			"seed_tracks" : seedTracks,
 			"seed_artists" : "",
 			"seed_genres" : "",
-			"target_valence" : params.valence
+            "target_valence" : params.valence,
+            "target_energy" : params.energy
 		},
 		})
 		.then((res) => {
-			// console.log(res.data)
-			tracks = res.data["tracks"]
+			let tracks = res.data["tracks"]
 			for(let i=0; i<tracks.length; i++){
-				newTracks.push(tracks[i]["uri"])
-				//console.log(tracks[i]["uri"])
+				filterednew.push(tracks[i]["uri"])
 			}
 		})
 		.catch((error) => {
-            // console.log(seed_track)
-            //console.error(error)
-            reject("Could not get recommended track")
+            reject("Could not get recommended tracks: \n" + error)
+            return
+        })
+        
+        let finalPlaylist = filteredold.concat(filterednew)
+
+
+        //Create the playlist
+        await axios.post('https://api.spotify.com/v1/users/' + user_name + '/playlists', {"name": playlist_name, "public": false}, {headers: {'Authorization' : 'Bearer ' + auth_token}})
+            .then((res) => {
+                playlist_id = res.data["id"]
+                playlist_uri = res.data["uri"]
+            })
+            .catch((error) => {
+                reject("Can't create a new playlist: \n" + error)
+                return
+        })
+
+        
+        //Add songs to playlist
+		await axios.post('https://api.spotify.com/v1/playlists/' + playlist_id + "/tracks", {"uris": finalPlaylist}, {headers: {'Authorization' : 'Bearer ' + auth_token}})
+		.catch((error) => {
+            reject("Couldn't add a song to the playlist: \n" + error)
             return
 		})
 
-        //Adds reccomended tracks
-		await axios.post('https://api.spotify.com/v1/playlists/' + playlist_id + "/tracks", {"uris": newTracks}, {headers: {'Authorization' : 'Bearer ' + auth_token}})
-		.then((res) => {
-			// //console.log(res.data)
-			//console.log("Added data")
-		})
-		.catch((error) => {
-			// //console.error(error)
-			//console.log("couldn't add song")
-			//console.log(newTracks)
-            // reject("Couldn't add the song to the playlist")
-            // return
-		})
-
-		// Adds initially found emotionally appropriate top tracks
-		await axios.post('https://api.spotify.com/v1/playlists/' + playlist_id + "/tracks", {"uris":emotionTopTracks}, {headers: {'Authorization' : 'Bearer ' + auth_token}})
-			.then((res) => {
-				//console.log(res.data)
-				//console.log("Added data")
-			})
-			.catch((error) => {
-                console.error(error)
-                console.log("can't add tracks")
-                // reject("Couldn't add the song to the playlist")
-                // return
-		})
+		
+		//Creates the playlist
+		await axios.post('https://api.spotify.com/v1/users/' + user_name + '/playlists', {"name": playlist_name, "public": false}, {headers: {'Authorization' : 'Bearer ' + auth_token}})
+            .then((res) => {
+                playlist_id = res.data["id"]
+                playlist_uri = res.data["uri"]
+            })
+            .catch((error) => {
+                reject("Couldn't create a new playlist: \n" + error)
+                return
+        })
+        
     
-        //Should set picture
+        //Set playlist image
         axios.put('https://api.spotify.com/v1/playlists/' + playlist_id + "/images", image_data, {headers: {'Authorization' : 'Bearer ' + auth_token, "Content-Type":"image/jpeg"}})
-        .then((res) => {
-            // console.log(res.data)
-        })
         .catch((error) => {
-            console.error(error)
-            console.log(image_data.length)
-            console.log("can't set picture")
+            reject("Couldn't set playlist image: \n" + error)
+            return
         })
-    
 
         resolve(playlist_uri)
-
+        
     })
 }
