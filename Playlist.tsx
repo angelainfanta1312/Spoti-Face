9001d09e92ce2a3a682acf551b770376333cfec6
David Buffkin <dbuffkin@andrew.cmu.edu>
2020-09-19 21:56:49 -0400

    Debug prompt for hopefully fixing detection

--- Playlist.tsx
+++ Playlist.tsx
@@ -1,12 +1,15 @@
 const axios = require('axios');
 
 // export default function createPlaylist(base64:string, face:any){
-export default function createPlaylist(image_data: string, sp: any, token){
+export default function createPlaylist(image_data: string, face: any, token){
     return new Promise(async (resolve, reject) => {
 
         //figure out TARGETPARAMS
         // []
-        sp = .99
+        console.log("Face passed to createPlaylist: " + JSON.stringify(face))
+        return
+
+        let sp = .99
                 
 
         // []
